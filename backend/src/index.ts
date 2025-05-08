5fef9ed69f29cf916c07dd73689a427be23f41dc
proHarshang <harshang.developer@gmail.com>
2025-05-09 00:09:09 +0530

    refactor [backend] : added url in cors to run without fetching url from .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials: true,
   })
 );
